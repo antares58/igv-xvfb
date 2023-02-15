c2b38da076f402c00f0908600369007f1da4aa14
Antares <94020256+antares58@users.noreply.github.com>
2023-02-15 13:26:32 -0800

    Make snapshotting work in a loop.

--- make_igv_snapshots.sh
+++ make_igv_snapshots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ${igvScriptDir}
 for i in *.igv ; do
-	/igv/IGV_Linux_2.16.0/igv.sh --batch /batchDir/${i}
+	/usr/bin/xvfb-run /igv/IGV_Linux_2.16.0/igv.sh --batch /batchDir/${i}
 done
