84330ceec6df4544c5fb90bf3765f2509fc7f41e
Antares <94020256+antares58@users.noreply.github.com>
2023-02-15 14:21:04 -0800

    Debugging snapshot loop.

--- make_igv_snapshots.sh
+++ make_igv_snapshots.sh
@@ -2,5 +2,5 @@
 cd ${igvScriptDir}
 for i in *.igv ; do
 	echo "Snapshot -> ${i}"
-	/usr/bin/xvfb-run /igv/IGV_Linux_2.16.0/igv.sh --batch ${i}
+	/usr/bin/xvfb-run /igv/IGV_Linux_2.16.0/igv.sh --batch /batchDir/${i}
 done
